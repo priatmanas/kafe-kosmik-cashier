2442f84991e38b353b7d3b7fc787a5dd22cc9a69
Fauzan Priatmana <fauzanpriatmanas@gmail.com>
2025-11-03 04:25:23 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author Fawzan Priatmana
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        menuProgram menuUtama = new menuProgram();
+        
+        menuUtama.setVisible(true);
+        
+    }
+    
+}
